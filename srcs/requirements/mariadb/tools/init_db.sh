b300fc9efd1e3318663e30ada66030a96a8f3745
hlimouni <hlimouni@penguin>
2021-09-14 02:46:07 +0100

    change root password command added to mysql config file

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -5,4 +5,20 @@ mkdir -p /run/openrc
 touch /run/openrc/softlevel
 /etc/init.d/mariadb setup
 rc-service mariadb start
+# mysql_secure_installation <<EOF
+
+# y
+# secret
+# secret
+# y
+# y
+# y
+# y
+# EOF
 mysql < /etc/scripts/configbase.sql
+# mysql <<EOF
+# ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_password';
+# EOF
+# mysql -e \
+# ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_password';
+# EOF
